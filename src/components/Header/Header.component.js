7dde92a07c6e9eeb7dd07ccc8861885cc5906035
nutsil <silpagui.sa@gmail.com>
2020-09-30 16:53:08 -0300

    Add Header back button

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./Header.styles.scss";
 
 export function Header() {
-  return <header>Nutella quiere salir</header>;
+  return (
+    <header>
+      <button className="header-back-button">
+        <img
+          src="http://placekitten.com/27/27"
+          alt="Back to home"
+          width="27"
+          height="27"
+        />
+      </button>
+    </header>
+  );
 }
