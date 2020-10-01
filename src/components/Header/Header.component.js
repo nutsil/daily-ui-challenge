0424921dab85ce5b712dc37754f5399b86bfb153
nutsil <silpagui.sa@gmail.com>
2020-09-30 23:17:17 -0300

    Add backArrow Icon on Header

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "./Header.styles.scss";
+import backArrowIconSRC from "../../assets/images/back-arrow.svg";
 
 export function Header() {
   return (
     <header>
       <button className="header-back-button">
-        <img
-          src="http://placekitten.com/27/27"
-          alt="Back to home"
-          width="27"
-          height="27"
-        />
+        <img src={backArrowIconSRC} alt="Back to home" width="27" height="27" />
       </button>
       <h1 className="header-section-title">Sign Up</h1>
     </header>
