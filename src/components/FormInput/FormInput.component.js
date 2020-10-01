41b6dac69241c2d5a2a2b43702b6e83ed495d68b
nutsil <silpagui.sa@gmail.com>
2020-09-30 22:38:14 -0300

    Wrap up SignUp form

--- src/components/FormInput/FormInput.component.js
+++ src/components/FormInput/FormInput.component.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./FormInput.styles.scss";
 
-export function FormInput() {
-  return <input type="text" placeholder="First Name" className="form-input" />;
+export function FormInput(props) {
+  return (
+    <input type={props.type} placeholder={props.label} className="form-input" />
+  );
 }
--- src/components/SignUpForm/SignUpForm.component.js
+++ src/components/SignUpForm/SignUpForm.component.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { FormInput } from "../FormInput/FormInput.component";
+import { RoundedButton } from "../RoundedButton/RoundedButton.component";
 import "./SignUpForm.styles.scss";
 
 export function SignUpForm() {
   return (
     <form>
-      <FormInput />
-      <FormInput />
-      <FormInput />
+      <FormInput type="text" label="First Name" />
+      <FormInput type="text" label="Last Name" />
+      <FormInput type="email" label="Your Email" />
+      <FormInput type="password" label="Password" />
+      <RoundedButton buttonText="Submit" />
     </form>
   );
 }
