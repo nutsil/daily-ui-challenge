cb0e6c7e9aa9e71af0757ad1dffe5b76daac52f0
nutsil <silpagui.sa@gmail.com>
2020-10-05 20:51:00 -0300

    Add FormInput className prop

--- src/components/FormInput/FormInput.component.js
+++ src/components/FormInput/FormInput.component.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./FormInput.styles.scss";
 
 export function FormInput(props) {
+  const className = `form-input ${props.className}`;
+
   return (
-    <input type={props.type} placeholder={props.label} className="form-input" />
+    <input type={props.type} placeholder={props.label} className={className} />
   );
 }
